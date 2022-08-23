a2e7de5f4cb7a0161d0890b6d7883c85bd2d124f
kabuiya <kabuiyamonicah@gmail.com>
2022-08-24 00:26:46 +0300

    initial commit

---
+++ currydrinks/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CurrydrinksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'currydrinks'
---
+++ currydrinks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Drinks(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
